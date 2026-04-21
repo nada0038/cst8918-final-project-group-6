85cc6c7bef9463868aae922b83ed09ac3731560e
nada0038 <nada0038@algonquinlive.com>
2026-04-21 10:02:17 -0400

    Auto-format applied for pipeline compliance

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -6,14 +6,14 @@ resource "azurerm_kubernetes_cluster" "aks" {
   kubernetes_version  = "1.32"
 
   default_node_pool {
-    name                 = "default"
-    vm_size              = var.vm_size
-    vnet_subnet_id       = var.subnet_id
-    
+    name           = "default"
+    vm_size        = var.vm_size
+    vnet_subnet_id = var.subnet_id
+
     enable_auto_scaling = var.max_count > var.min_count ? true : false
-    node_count           = var.node_count
-    min_count            = var.max_count > var.min_count ? var.min_count : null
-    max_count            = var.max_count > var.min_count ? var.max_count : null
+    node_count          = var.node_count
+    min_count           = var.max_count > var.min_count ? var.min_count : null
+    max_count           = var.max_count > var.min_count ? var.max_count : null
   }
 
   identity {
